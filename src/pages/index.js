f826556f48648767586411509b79404cd61bf52b
IsmailSossiAlaoui <sossiismail@gmail.com>
2023-07-25 14:13:05 +0100

    pdf update

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
           Explore my latest projects, showcasing my expertise in React.js, ux/ui design, and web development.
           </p>
           <div className='flex items-center self-start mt-2 lg:self-center'> 
-        <Link href={"/public/Ismail_Sossi_Alaoui_cv.pdf"} target='_blank' className="dark:hover:bg-dark dark:hover:text-light dark:hover:border-light dark:bg-light dark:text-dark flex items-center border-dark border transition-all ease-in-out border-solid boder-2 bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-transparent hover:border-dark whitespace-nowrap" download={true}>My resumé<LinkArrow className="w-6 ml-1 " /></Link>
+        <Link href={"/Ismail_Sossi_Alaoui_cv.pdf"} target='_blank' className="dark:hover:bg-dark dark:hover:text-light dark:hover:border-light dark:bg-light dark:text-dark flex items-center border-dark border transition-all ease-in-out border-solid boder-2 bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-transparent hover:border-dark whitespace-nowrap" download={true}>My resumé<LinkArrow className="w-6 ml-1 " /></Link>
         <Link href={"mailto:sossiismail@gmail.com"} target='_blank' className='dark:text-light dark:border-light dark:hover:bg-light dark:hover:text-dark dark:hover:border-dark text-dark transition-all ease-in-out  rounded-lg font-semibold hover:bg-dark hover:text-light border-2 border-dark p-2.5 px-6 ml-3 '>Contact</Link>
         </div>
       </div>
